3ca383699a1b554afb03dfeef7104e4054e32d45
ubk8751 <ubk8751@gmail.com>
2017-04-07 10:40:48 +0200

    Add files via upload

---
+++ Nathsida/LLLJS.js
@@ -0,0 +1,22 @@
+var ant = 0;
+function changeImage(){
+		ant++;
+		if(ant == 5){
+			ant = 0;
+			}
+		if(ant == 0){
+			document.getElementById('#artshowcase').src="warm_at_heart_by_littlelionlie-daj716n.jpg";
+		}
+		else if(ant == 1){
+			document.getElementById('artshowcase').src="fire_by_littlelionlie-db1fcg8.jpg";
+			}
+		else if(ant == 2){
+			document.getElementById('artshowcase').src="alpaca_love_by_littlelionlie-daoq251.jpg";
+			}
+		else if(ant == 3){
+			document.getElementById('artshowcase').src="camouflage_by_littlelionlie-daa7fg4.jpg";
+			}
+		else if(ant == 4){
+			document.getElementById('artshowcase').src="through_frames_by_littlelionlie-d9btro8.jpg";
+			}	
+		}
